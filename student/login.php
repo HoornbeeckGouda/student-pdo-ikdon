a990659f1f0a9205de11efbc8e63d8f30550abe5
ikdon <donhaohu@gmail.com>
2024-04-11 16:02:11 +0200

    Removed whitespace and added comment for password

--- student/login.php
+++ student/login.php
@@ -3,9 +3,11 @@ include 'inc/header.php';
 if (isset($_SESSION['ingelogd']) && $_SESSION['ingelogd'] === true) {
     session_destroy();
 }
+// Administratie wachtwoord = Administratie
+// Docent wachtwoord = Docent
 ?>
     <form action="inloggen.php" method="post">
-        <input type="text" id="inlognaam" name="inlognaam" placeholder="Inlognaam   " required>
+        <input type="text" id="inlognaam" name="inlognaam" placeholder="Inlognaam" required>
         <input type="password" id="wachtwoord" name="wachtwoord" placeholder="Wachtwoord" required>
         <input type="submit" name="submit" value="Inloggen">
     </form>
